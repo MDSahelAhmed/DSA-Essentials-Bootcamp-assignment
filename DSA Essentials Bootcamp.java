a505e2c7814e4f1285c1f2f6611ee82402508932
Mohammad Sahel Ahmed <sahelahmed975@gmail.com>
2023-08-22 16:10:19 +0530

    Update DSA Essentials Bootcamp.java

--- DSA Essentials Bootcamp.java
+++ DSA Essentials Bootcamp.java
@@ -1,1 +1,26 @@
+public class MaxDifference {
+
+    public static int largestDifference(int[] arr) {
+        if (arr.length < 2) {
+            return 0; 
+        }
+
+        int minElement = arr[0];
+        int maxDifference = 0;
+
+        for (int num : arr) {
+            int difference = num - minElement;
+            maxDifference = Math.max(maxDifference, difference);
+            minElement = Math.min(minElement, num);
+        }
+
+        return maxDifference;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 9, 5, 6, 3, 2};
+        int output = largestDifference(arr);
+        System.out.println(output);  
+    }
+}
 
